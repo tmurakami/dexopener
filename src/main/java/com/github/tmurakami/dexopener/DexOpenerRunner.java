05b23283f3e819c1e8353e1e8a3156638b3ae663
tmurakami <tmurakami@users.noreply.github.com>
2017-01-22 18:44:57 +0900

    Change minSdkVersion to 16

--- src/main/java/com/github/tmurakami/dexopener/DexOpenerRunner.java
+++ src/main/java/com/github/tmurakami/dexopener/DexOpenerRunner.java
@@ -3,7 +3,6 @@ package com.github.tmurakami.dexopener;
 import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.os.Bundle;
 import android.support.test.runner.AndroidJUnitRunner;
 
 import com.github.tmurakami.classinjector.ClassInjector;
@@ -17,27 +16,14 @@ import java.util.logging.Logger;
  */
 public class DexOpenerRunner extends AndroidJUnitRunner {
 
-    private boolean initialized;
-
-    @Override
-    public void onCreate(Bundle arguments) {
-        init();
-        super.onCreate(arguments);
-    }
-
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-        // On Android 4.1 or later, this is called before onCreate.
         init();
         return super.newApplication(cl, className, context);
     }
 
     private void init() {
-        if (initialized) {
-            return;
-        }
-        initialized = true;
         Context context = getTargetContext();
         ApplicationInfo ai = context.getApplicationInfo();
         File cacheDir = new File(ai.dataDir, "code_cache/dexopener");
