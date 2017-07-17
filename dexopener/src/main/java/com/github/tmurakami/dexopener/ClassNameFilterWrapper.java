340a728cb061b422722cec08e2562c32d613aa4c
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-07-18 02:45:46 +0900

    Fixes #8: Fix to exclude all classes under the package `android.databinding`

--- dexopener/src/main/java/com/github/tmurakami/dexopener/ClassNameFilterWrapper.java
+++ dexopener/src/main/java/com/github/tmurakami/dexopener/ClassNameFilterWrapper.java
@@ -4,18 +4,6 @@ import android.support.annotation.NonNull;
 
 final class ClassNameFilterWrapper implements ClassNameFilter {
 
-    private static final String[] INCLUDED_PACKAGES = {
-            "android.databinding.generated.",
-    };
-
-    private static final String[] INCLUDED_CLASSES = {
-            // Since the Data Binding Library generates several classes that are tightly coupled
-            // with user classes, do not exclude the following classes.
-            "android.databinding.DataBinderMapper",
-            "android.databinding.DataBindingComponent",
-            "android.databinding.DataBindingUtil",
-    };
-
     private static final String[] EXCLUDED_PACKAGES = {
             "android.",
             "com.android.",
@@ -44,16 +32,6 @@ final class ClassNameFilterWrapper implements ClassNameFilter {
 
     @Override
     public boolean accept(@NonNull String className) {
-        for (String pkg : INCLUDED_PACKAGES) {
-            if (className.startsWith(pkg)) {
-                return true;
-            }
-        }
-        for (String cls : INCLUDED_CLASSES) {
-            if (className.equals(cls)) {
-                return true;
-            }
-        }
         for (String pkg : EXCLUDED_PACKAGES) {
             if (className.startsWith(pkg)) {
                 return false;
