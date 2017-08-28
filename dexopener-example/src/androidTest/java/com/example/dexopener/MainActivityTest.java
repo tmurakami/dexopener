cfad16d3db8ef551d92bbf60a50c38b0a991d286
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-08-28 19:13:12 +0900

    Refactor MainActivityTest to be more modern

--- dexopener-example/src/androidTest/java/com/example/dexopener/MainActivityTest.java
+++ dexopener-example/src/androidTest/java/com/example/dexopener/MainActivityTest.java
@@ -1,55 +1,38 @@
 package com.example.dexopener;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.lifecycle.ActivityLifecycleCallback;
-import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
-import android.support.test.runner.lifecycle.Stage;
+import android.support.test.runner.intercepting.SingleActivityFactory;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class MainActivityTest implements ActivityLifecycleCallback {
+public class MainActivityTest {
 
     @Rule
     public final ActivityTestRule<MainActivity> activityRule =
-            new ActivityTestRule<>(MainActivity.class, true, false);
+            new ActivityTestRule<>(new SingleActivityFactory<MainActivity>(MainActivity.class) {
+                @Override
+                protected MainActivity create(Intent intent) {
+                    MainActivity testTarget = new MainActivity();
+                    testTarget.delegate = mock(MainActivityDelegate.class);
+                    return testTarget;
+                }
+            }, /* initialTouchMode */ false, /* launchActivity */ false);
 
     @Rule
     public final MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
-    @Mock
-    private MainActivityDelegate delegate;
-
-    @Before
-    public void setUp() throws Exception {
-        ActivityLifecycleMonitorRegistry.getInstance().addLifecycleCallback(this);
-        activityRule.launchActivity(null);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        ActivityLifecycleMonitorRegistry.getInstance().removeLifecycleCallback(this);
-    }
-
-    @Override
-    public void onActivityLifecycleChanged(Activity activity, Stage stage) {
-        if (activity instanceof MainActivity && stage == Stage.PRE_ON_CREATE) {
-            ((MainActivity) activity).delegate = delegate;
-        }
-    }
-
     @Test
     public void onCreate_should_simply_call_MainActivityDelegate_onCreate() throws Exception {
-        verify(delegate).onCreate(null);
+        activityRule.launchActivity(null);
+        verify(activityRule.getActivity().delegate).onCreate(null);
     }
 
 }
